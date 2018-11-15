67dcc0430801d5e727c4fb7eb1da80e758945e4f
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-11-14 23:21:40 -0600

    Module 2 Complete - BUG: No NProgress

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import Link from 'next/link';
 
-// class Home extends React.Component {
-// 	render() {
-// 		return <p>Hey!</p>;
-// 	}
-// }
-
 const Home = (props) => (
 	<div>
 		<h1>This is the Home Page</h1>
-		<Link href="/sell">
-			<a>Click to go to Sell</a>
-		</Link>
 	</div>
 );
 
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -4,9 +4,6 @@ import Link from 'next/link';
 const Sell = (props) => (
 	<div>
 		<h1>This is the sell page!</h1>
-		<Link href="/">
-			<a>Click to go to Sell</a>
-		</Link>
 	</div>
 );
 
